797f649d954dfecd0df1054cc854ac01ff857c40
127.0.0.1 <125826136+MJMODZZ-V3@users.noreply.github.com>
2023-06-08 00:48:24 +0200

    🎆 Eclipse RAT Source 🎆

---
+++ Holy/KeyAuth-VB-Example-main/Auth.vb
@@ -0,0 +1,66 @@
+﻿Imports KeyAuth_VB.KeyAuth
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        KeyAuthApp.init()
+        Label2.Visible = False
+    End Sub
+    Private Shared name As String = "" ' Application name, found in dashboard
+    Private Shared ownerid As String = "" ' Ownerid, found in account settings of dashboard
+    Private Shared secret As String = "" ' Application name, found in dashboard. It's the blurred text beneath application name
+    Private Shared version As String = "1.0"
+    Public Shared KeyAuthApp As api = New api(name, ownerid, secret, version)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If KeyAuthApp.login(TextBox1.Text, TextBox2.Text) Then
+            Button1.Visible = False
+            Button2.Visible = False
+            Button3.Visible = False
+            Button4.Visible = False
+            Label1.Visible = False
+            Label2.Visible = False
+            Label3.Visible = False
+            Label4.Visible = False
+            TextBox1.Visible = False
+            TextBox2.Visible = False
+            TextBox3.Visible = False
+            Label2.Visible = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If KeyAuthApp.register(TextBox1.Text, TextBox2.Text, TextBox3.Text) Then
+            Button1.Visible = False
+            Button2.Visible = False
+            Button3.Visible = False
+            Button4.Visible = False
+            Label1.Visible = False
+            Label2.Visible = False
+            Label3.Visible = False
+            Label4.Visible = False
+            TextBox1.Visible = False
+            TextBox2.Visible = False
+            TextBox3.Visible = False
+            Label2.Visible = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        KeyAuthApp.upgrade(TextBox1.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If KeyAuthApp.license(TextBox3.Text) Then
+            Button1.Visible = False
+            Button2.Visible = False
+            Button3.Visible = False
+            Button4.Visible = False
+            Label1.Visible = False
+            Label2.Visible = False
+            Label3.Visible = False
+            Label4.Visible = False
+            TextBox1.Visible = False
+            TextBox2.Visible = False
+            TextBox3.Visible = False
+            Label2.Visible = True
+        End If
+    End Sub
+End Class
---
+++ Holy/KeyAuth-VB-Example-main/README.md
@@ -0,0 +1,16 @@
+# Archived:
+
+Due to the low usage ofthe VB.net SDK, and the redundancy of it, it has been archived.
+
+You may convert the C# SDK to VB.net using https://github.com/dnSpy/dnSpy 
+
+# KeyAuth-VB-Example
+KeyAuth Visual Basic Example For The https://keyauth.win Authentication system.
+
+Credits to Duck!#4633 (https://github.com/DuckIsCool) for authoring base
+
+Change 3 strings in Form1.vb and you're ready.
+
+**What is KeyAuth?**
+
+KeyAuth is an Open source authentication system with cloud hosting plans as well. Client SDKs available for C++, C#, Python, Rust, PHP, and VB.NET. KeyAuth several unique features such as memory streaming, webhook function where you can send requests to API without leaking the API, discord webhook notifications, ban the user securely through the application at your discretion. Feel free to join https://keyauth.win/discord/ if you have questions or suggestions.
